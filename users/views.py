f33d2afd1d827e20b10426ddb0cbf115caf7a289
Otajon Ravilov <otajonravilov2008@gmail.com>
2025-03-13 12:33:50 +0500

    first commit

--- users/views.py
+++ users/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, redirect
-
-
-
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def login_page(request):
-    return render(request, 'users/login.html')
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(request, username=user.username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                messages.error(request, "Email yoki parol noto‘g‘ri!")
+        except User.DoesNotExist:
+            messages.error(request, "Bunday email mavjud emas!")
+
+    return render(request, "users/login.html")
 
 
 
@@ -16,5 +31,4 @@ def register_page(request):
 
 
 def logout_page(request):
-    return render()
-pass
+    pass
